c262e71912ee78a80106cd9b7b1eeea886eedb73
lbezel <bezellisa@yandex.ru>
2013-09-10 21:21:35 +0400

    ex1.py

---
+++ lbezel/2/ex1.py
@@ -0,0 +1,27 @@
+
+'''a= int(input())
+n= int(input())
+l=a
+for i in range(1, n+1):
+	l=l*l
+print(l)'''
+'''
+from math import sqrt
+a= int(input())
+n= int(input())
+o=n
+p=1
+l=0
+k=0	
+poi=1
+y=a
+while n != 0:
+	l=n%2
+	
+	if l==1:
+		poi=poi*y
+	k=k+1
+	n=n//2
+	
+	y=y*y
+print(poi)'''

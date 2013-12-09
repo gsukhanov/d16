cb8ee43d6f7820332312947fcbe97c1b38e8a4e5
KRyzhkov <ryzhkir@mail.ru>
2013-12-09 10:47:07 +0400

    Arcanoid

--- kryzhkov/Arcanoid/Arcanoid.py
+++ kryzhkov/Arcanoid/Arcanoid.py
@@ -6,22 +6,21 @@ from collections import namedtuple
 WIDTH=660
 HEIGHT=660
 FPS = 1000
-RADIUS = 1000
- 
+RADIUS = 10
+  
  
 Point = namedtuple("Point", ["x", "y"])
 Game = namedtuple("Game", ["position", "direction","blocks","field"])
 
 def draw_field(canvas, game):
-  cell_width = WIDTH // 21
+  cell_width = WIDTH // 22
   cell_height = cell_width
   canvas.delete(*canvas.find_all())
-  canvas.create_oval(
+  canvas.create_rectangle(
       game.position.x-RADIUS,
       game.position.y-RADIUS,
       game.position.x+RADIUS,
-      game.position.y+RADIUS,
-      fill="red")
+      game.position.y+RADIUS,fill="red")
   def draw_cell(point, color):
     canvas.create_rectangle(
         cell_width * point.x,
@@ -30,9 +29,7 @@ def draw_field(canvas, game):
         cell_height * (point.y + 1),
         fill=color)
 
-  canvas.delete(*canvas.find_all())
-
-  for i in range(21):
+  for i in range(22):
     for j in range(3):
       draw_cell(Point(i, j),"white")
   for p in game.blocks:
@@ -49,30 +46,39 @@ def make_move(game):
     direction = direction._replace(x=-direction.x)
   if not (RADIUS < position.y < HEIGHT-RADIUS):
     direction = direction._replace(y=-direction.y)
-  if  (position.y+RADIUS in game.blocks) and not position.y+RADIUS in game.field:
+  if  (position.y+RADIUS in blocks) and not position.y+RADIUS in field:
     direction = direction._replace(y=-direction.y)
-    return game._replace(
-      blocks=(game.blocks - frozenset([position])),
-
-      field=(position,) + game.field)
-  if  position.y-RADIUS in game.blocks and not position.y-RADIUS in game.field:
+    return Game(
+        position=position,
+        direction=direction,  
+        blocks=(game.blocks - frozenset([position])),
+        field=(position,) + game.field)
+  if  position.y-RADIUS in blocks and not position.y-RADIUS in field:
     direction = direction._replace(y=-direction.y)
-    return game._replace(
-      blocks=(game.blocks - frozenset([position])),
-
-      field=(position,) + game.field)
-  if  position.x+RADIUS in game.blocks and not position.x+RADIUS in game.field:
+    return Game(
+        position=position,
+        direction=direction,  
+        blocks=(game.blocks - frozenset([position])),
+        field=(position,) + game.field)
+  if  position.x+RADIUS in blocks and not position.x+RADIUS in field:
     direction = direction._replace(x=-direction.x)
-    return game._replace(
-      blocks=(game.blocks - frozenset([position])),
-
-      field=(position,) + game.field)
-  if  position.x-RADIUS in game.blocks and not position.x-RADIUS in game.field:
+    return Game(
+        position=position,
+        direction=direction,  
+        blocks=(game.blocks - frozenset([position])),
+        field=(position,) + game.field)
+  if  position.x-RADIUS in blocks and not position.x-RADIUS in field:
     direction = direction._replace(x=-direction.x)
-    return game._replace(
-      blocks=(game.blocks - frozenset([position])),
-      field=(position,) + game.field)
-  return Game(position, direction,blocks,field)
+    return Game(
+        position=position,
+        direction=direction,  
+        blocks=(game.blocks - frozenset([position])),
+        field=(position,) + game.field)
+  return Game(
+        position=position,
+        direction=direction,
+        blocks=blocks,
+        field=field)
  
         
  
@@ -88,12 +94,12 @@ def loop():
   loop.game = make_move(loop.game)
   c.after(1000 // FPS, loop)
  
-loop.game = Game(Point(WIDTH/2,HEIGHT/2), 
+loop.game = Game(Point(WIDTH/2,HEIGHT/2),
     Point(1,2),
     blocks=(
         [Point(x, y)
         for y in range(3)
-          for x in range(21)]),
+          for x in range(22)]),
     field=frozenset())
  
 loop()

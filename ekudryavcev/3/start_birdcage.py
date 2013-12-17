f5d229868be86541fe2e0d2f553c06e9f27c2efd
ekudryavcev <keremey@mail.ru>
2013-12-17 10:44:47 +0400

    And still in progress...

--- ekudryavcev/3/start_birdcage.py
+++ ekudryavcev/3/start_birdcage.py
@@ -2,9 +2,10 @@ import tkinter
 from collections import namedtuple
 
 Bridge = namedtuple("Bridge", ["xpos", "ypos", "player", "direction"])
+dirct = 0
 
 print("Input the size of the cage in dots width; from 6 to 15")
-size = int(input())
+size = 8
 if size < 6 or size > 15:
 	print("Wrong size, restart the game")
 c = tkinter.Canvas(width = 100 + 50 * size, height = 100 + 50 * size)
@@ -38,17 +39,40 @@ def bridgeit(b , c):
 		c.create_line(b.xpos , b.ypos , b.xpos , b.ypos + 50 , fill = b.player , width = 3)
 
 def click(event):
+	global playermove
+	global dirct
 	c = event.widget
-	xp = event.x
-	yp = event.y
-	nex = (xp - 25) / 25
-	ney = (yp - 50) / 25
-	b = Bridge(nex * 25 + 25 , ney * 25 + 50 , "red" , "hor")
+	xp = event.x - 25
+	yp = event.y - 50
+	nex = xp // 25
+	ney = yp // 25
+	print(nex, ney)
+	sumx = nex + ney
+	sumy = nex - ney + 2
+	if int((sumx + sumy)) % 4 == 0 and int((sumx - sumy)) % 4 == 0:
+		if playermove == "red":
+			dirct = "vert"
+			nex += - 1
+			ney += 1
+		if playermove == "blue":
+			dirct = "hor"
+	if int((sumx + sumy)) % 4 == 2 and int((sumx - sumy)) % 4 == 2:
+		if playermove == "blue":
+			dirct = "vert"
+		if playermove == "red":
+			dirct = "hor"
+	b = Bridge(nex * 25 + 25 , ney * 25 + 50 , playermove , dirct)
 	bridges.append(b)
 	bridgeit(b , c)
+	#print(xp , yp , nex , ney , b.xpos , b.ypos)
+	if playermove == "red":
+		playermove = "blue"
+	else:
+		playermove = "red"
 
 cancrea(c)
 
+playermove = "red"
 c.bind("<Button-1>", click)
 
 bridges = []

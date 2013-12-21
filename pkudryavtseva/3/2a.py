dfc0dc115d657849a171968767e5c96e8ec0c023
«pkudryavtseva» <kupoly@mail.ru>
2013-12-21 22:14:54 +0300

    Nice solution for 2a ^_^

---
+++ pkudryavtseva/3/2a.py
@@ -0,0 +1,22 @@
+n = int(input())
+a = []
+for i in range(1,n+1):
+  y = int(input())
+  a.append(y)
+x = a
+for i in range(n):
+  for m in range(n):
+    if b[m] < b[m + 1]:
+      b[m] = 1
+      b[m + 1] = 0    
+k=0
+while a != x:
+  p = 0
+  while p < len(a)-1:
+    if a[p] < a[p+1]:
+      a[p] = 1
+      a[p+1] = 0
+      p = p + 1
+    p = p+1
+  k += 1
+print(k)

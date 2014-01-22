6ef8b4cea13b0e269f159424a9a8fe39d4ad36a8
eshuvaeva <Elizshuvaeva@yandex.ru>
2014-01-22 19:54:22 +0400

    Puts pieces in right places

--- eshuvaeva/newren.py
+++ eshuvaeva/newren.py
@@ -17,8 +17,8 @@ whitey = []
 blacky = []
 #field
 for x in range(0, N * SQUARE, SQUARE):
-	for y in range(0, N * SQUARE, SQUARE):
-		c.create_rectangle( x , y , x + SQUARE,  y + SQUARE, fill = "saddle brown")
+    for y in range(0, N * SQUARE, SQUARE):
+        c.create_rectangle( x , y , x + SQUARE,  y + SQUARE, fill = "saddle brown")
 
 #functions
 
@@ -39,12 +39,22 @@ def change_into_black():
 def location(event): 
     c = event.widget 
     global v
-    for k in range(M + 1):
-        for z in range(M + 1):
+    for k in range(M ):
+        for z in range(M ):
             xy = coordinate(event.x, event.y)
             if xy[0] == k and xy[1] == z  :
-                print(xy[0], xy[1], k, z)
-                c.create_oval(((k/2 - 1) * SQUARE + SQUARE/2) , ( (z/2 -1) * SQUARE + SQUARE/2), ( ( k/2 + 1) * SQUARE - SQUARE/2),((z/2 + 1) * SQUARE - SQUARE/2), fill = previous_color)
+
+                if k % 2 == 0 and z % 2 == 0:
+                    c.create_oval(((k/2 - 0.5) * SQUARE) , ( (z/2 -0.5) * SQUARE ), ( ( k/2 + 0.5) * SQUARE),((z/2 + 0.5) * SQUARE ), fill = previous_color)
+
+                if k % 2 == 1 and z % 2 == 0:
+                    c.create_oval((((k -1)/2 + 0.5) * SQUARE) , ( (z/2 -0.5) * SQUARE ), ( ( (k - 1)/2 + 1.5) * SQUARE),((z/2 + 0.5) * SQUARE), fill = previous_color)
+                    print((k/2 + 0.5) * SQUARE, (k/2 + 1.5) * SQUARE)
+                if k % 2 == 0 and z % 2 == 1:
+                    c.create_oval(((k/2 - 0.5) * SQUARE) , ( ((z - 1)/2 + 0.5) * SQUARE), ( ( k/2 + 0.5) * SQUARE),(((z - 1)/2 + 1.5) * SQUARE), fill = previous_color)
+                
+                if k % 2 == 1 and z % 2 == 1:
+                    c.create_oval((((k - 1)/2 + 0.5) * SQUARE) , ( ((z - 1)/2 + 0.5) * SQUARE ), ( (( k - 1)/2 + 1.5) * SQUARE ),(((z - 1)/2 + 1.5) * SQUARE), fill = previous_color)
                 v = v + 1
                 if v % 2 == 1:
                     change_into_white()
@@ -59,3 +69,4 @@ def move(event):
 c.bind("<Button-1>", move) 
  
 c.mainloop()
+

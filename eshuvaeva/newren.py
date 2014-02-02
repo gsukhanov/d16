fe61f8df45573862fb5b1bacaad8bed131907f61
eshuvaeva <Elizshuvaeva@yandex.ru>
2014-02-02 21:05:49 +0400

    rendzu improved

--- eshuvaeva/newren.py
+++ eshuvaeva/newren.py
@@ -10,50 +10,40 @@ p = 22.5
 #variables
 previous_color = 'black'
 v = 0
-
 #arrays
+white = []
 black = []
-
 #field
 for x in range(0, N * SQUARE, SQUARE):
     for y in range(0, N * SQUARE, SQUARE):
         c.create_rectangle( x , y , x + SQUARE,  y + SQUARE, fill = "saddle brown")
 
-#temporary functions
-
-def write(event): #эта функция и будет вызываться. На вход ей будет подаваться информация о событии
-    c = event.widget #в частности, передаётся ссылка на объект
-    c.create_text(event.x, event.y, text=coordinate(event.x, event.y)) 
-def last(k, z):
-    if k == 28:
-        k = k -1
-    if z == 28:
-        z = z - 1
-    return(k,z)
-def first(k, z):
-    if k == 0:
-        k = k + 1
-    if z == 0:
-        z = z + 1
-    return(k,z)
-
 #functions
+
 def coordinate(m, k):
     m =  m // p
     k = k // p
     return m, k
 
+def change_into_white():
+    global previous_color
+    previous_color = 'white'
+    
+def change_into_black():
+    global previous_color
+    previous_color = 'black'
 
 def vertical_match(array):
-    for i in range(len(array)):
-        h = 1
-        for j in range(len(array)):
-            if array[i][0] == array[j][0] and i != j:
-                h = h + 1
-                if h == 5:
+    k = 1
+    for i in range(0, len(array), 2):
+        for j in range(0, len(array), 2):
+            if array[i] == array[j] and i != j:
+                k = k + 1
+                if k == 5:
                     text = c.create_text(700, 100, text = 'you win', fill = "red")
 
 
+    
 
 def location(event): 
     c = event.widget 
@@ -63,21 +53,23 @@ def location(event):
             xy = coordinate(event.x, event.y)
             a = xy[0] // 2
             b = xy[1] // 2
-            inside = []
-            inside.append(a)
-            inside.append(b)
             if xy[0] == k and xy[1] == z : 
-                if k % 2 == 0 and z % 2 == 0:
-                    c.create_oval(((k/2 - 0.5) * SQUARE) , ( (z/2 -0.5) * SQUARE ), ( ( k/2 + 0.5) * SQUARE),((z/2 + 0.5) * SQUARE ), fill = previous_color)
-                if k % 2 == 1 and z % 2 == 0:
-                    c.create_oval((((k -1)/2 + 0.5) * SQUARE) , ( (z/2 -0.5) * SQUARE ), ( ( (k - 1)/2 + 1.5) * SQUARE),((z/2 + 0.5) * SQUARE), fill = previous_color)
-                if k % 2 == 0 and z % 2 == 1:
-                    c.create_oval(((k/2 - 0.5) * SQUARE) , ( ((z - 1)/2 + 0.5) * SQUARE), ( ( k/2 + 0.5) * SQUARE),(((z - 1)/2 + 1.5) * SQUARE), fill = previous_color)
-                if k % 2 == 1 and z % 2 == 1:
-                    c.create_oval((((k - 1)/2 + 0.5) * SQUARE) , ( ((z - 1)/2 + 0.5) * SQUARE ), ( (( k - 1)/2 + 1.5) * SQUARE ),(((z - 1)/2 + 1.5) * SQUARE), fill = previous_color)
-                black.append(inside)
+                one = (k/2*SQUARE + SQUARE/2) // SQUARE
+                two = (z/2*SQUARE + SQUARE/2) // SQUARE
+                c.create_oval((one * SQUARE  - SQUARE/2 ) , ( two * SQUARE - SQUARE/2 ), (one * SQUARE + SQUARE/2),( two * SQUARE + SQUARE/2), fill = previous_color)
+                print(white) 
                 print(black)
-                vertical_match(black)
+                v = v + 1
+                if v % 2 == 1:
+                    black.append(a)
+                    black.append(b)
+                    vertical_match(black)
+                    change_into_white()
+                else:
+                    white.append(a)
+                    white.append(b)
+                    vertical_match(black)
+                    change_into_black()
 def move(event): 
     c = event.widget 
     location(event)
@@ -85,7 +77,6 @@ def move(event):
 
 
 c.bind("<Button-1>", move) 
-
-c.bind("<Button-3>", write) 
+ 
 c.mainloop()
 

8c1c8a7bf839dd645c6f2ecb8900fb3d3d271a50
eshuvaeva <Elizshuvaeva@yandex.ru>
2014-02-09 22:46:41 +0400

    Tsak 2

--- eshuvaeva/stringsandfiles/1.4.py
+++ eshuvaeva/stringsandfiles/1.4.py
@@ -1,17 +1,18 @@
 name = str(input())
-k = int(input())
 m = 0
-string = name
 f_read = open(name, 'r')
-for line in f_read:
-    line = line.strip()
-    for i in range(len(string)):
-    	s = name.find(" ")
-    	string1 = string[:s]
-    	for k in range(len(string1)//2):
-    		if string[n] == string[-n]:
-    			m = m + 1
-    	string = string[s:]
-
-    print(m)
+for line in f_read.readlines():
+	line = line.strip()
+	print(line)
+	for i in range(len(line)):
+		s = line.find(" ")
+		if s == -1:
+			print(m)
+			break
+		else:
+			k = line[:s]
+			if k == k[::-1]:
+				m = m + 1
+			line = line[(s + 1):]
+print(m)
 f_read.close()

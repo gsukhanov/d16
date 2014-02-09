c08486d00521346666b0a1735dc35f056ec66109
eshuvaeva <Elizshuvaeva@yandex.ru>
2014-02-09 23:09:23 +0400

    Task 1

--- eshuvaeva/stringsandfiles/1.1.py
+++ eshuvaeva/stringsandfiles/1.1.py
@@ -4,12 +4,10 @@ m = 0
 f_read = open(name, 'r')
 for line in f_read.readlines():
 	line = line.strip()
-	print(line)
 	for i in range(len(line)):
 		s = line.find(" ")
 		if k == s:
 			m = m + 1
 		line = line[(s + 1):]
-print(s)
 print(m)
 f_read.close()

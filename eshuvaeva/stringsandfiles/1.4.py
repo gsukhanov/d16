0b5f828e05e0d2a374c933b0382c2d5ee7cc4f0d
eshuvaeva <Elizshuvaeva@yandex.ru>
2014-02-13 21:28:47 +0400

    The split function did help

--- eshuvaeva/stringsandfiles/1.4.py
+++ eshuvaeva/stringsandfiles/1.4.py
@@ -3,16 +3,11 @@ m = 0
 f_read = open(name, 'r')
 for line in f_read.readlines():
 	line = line.strip()
-	print(line)
-	for i in range(len(line)):
-		s = line.find(" ")
-		if s == -1:
-			print(m)
-			break
-		else:
-			k = line[:s]
-			if k == k[::-1]:
-				m = m + 1
-			line = line[(s + 1):]
+	s = line.split(" ")
+	print(s)
+	for j in range(len(s)):
+		word = s[j]
+		if word == word[::-1]:
+			m = m + 1
 print(m)
 f_read.close()
